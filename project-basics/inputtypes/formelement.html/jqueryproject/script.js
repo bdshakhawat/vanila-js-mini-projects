cae1abe51bd1a12677e8fd90e0162b76270766b8
Your Name <you@example.com>
2021-04-28 22:49:06 +0600

    one commit

---
+++ project-basics/inputtypes/formelement.html/jqueryproject/script.js
@@ -0,0 +1,119 @@
+
+// in any function easing effect will work after giving the duration value like: .hide(1000, 'easeInOutElastic')
+
+
+// $(document).ready(function(){
+//     $(".box").animate({"height" : "400px", 'width' : '400px'}, 3000, 'easeInOutElastic', function(){
+//         $(this).css('background','green');
+//     });
+// });
+
+// $(document).ready(function(){
+//     $(".html").animate({'scrollTop': '0'}, 2000);
+// });
+// $(document).ready(function(){
+//     $("p").text('<h2>The text method changed the text of p</h2>');
+// });
+
+
+
+/*  .html() method: This method is also used to change the content or text of any text inside a html tag.
+like .text()  method but the only difference between .html() and .text() is that .text() can show 
+anything you write inside the () including html tags. but .html() cannot show the tags rather it apply this tags.
+$('document').ready(function(){
+    ** the text will be also bold**
+        $('p').html("<bold> Change the text</bold>");
+
+}); */
+
+
+
+/* .append(): This method will add text after existing text
+$('document').ready(function(){
+        $("p").append('Add text after p');
+
+        output: This is a text. Add text after p.
+
+}); */
+
+
+
+/* .prepend(): This method will add text before existing text
+$('document').ready(function(){
+        $("p").prepend('Add text before p');
+});
+
+ output: This is a text. Add text after p.*/
+
+
+ /*.after():A new paragraph mypara1  will be added after #p1
+  var mypara1 = $('<p></p>').text('This is paragraph1')
+ $('document').ready(function(){
+     $('#p1').after(mypara1);
+
+ });
+
+
+ /* .before():A new paragraph mypara1  will be added after #p1var mypara1 = $('<p></p>').text('This is paragraph1')
+ $('document').ready(function(){
+     $('#p1').after(mypara1);
+
+ });*/
+
+
+ /*.attr: with this method we can see the value of the attribute used in a particular tag
+ $('document').ready(function(){
+     $('a').attr('href');
+ });
+
+ output: http://google.com ***we can see the value of attribute*** */
+
+
+/*.removeAttr(): With this method we can remove the value of attribute from the html tag.
+$('document').ready(function(){
+     $('a').removeAttr('href');
+ });
+
+ output: undefined */
+
+/* with javascript: document.querySelector("a").setAttribute('href', 'https://www.studywithanis.com');
+
+.attr: with this method we can change the value of the attribute used in a particular tag
+ $('document').ready(function(){
+     $('a').attr('href', 'https://www.studywithanis.com');
+ });  */
+
+
+/*  ********Manipulating CSS*******
+ with this .css('property', 'value'); we can add css or style html tags.
+
+$('h1').css('color', 'red'); 
+or, $('h1').css({'color': 'red', 'font-size': '10px', 'font-weight':'2rem', 'font-family':'tahoma'});we can use multiple valu as jsoN value or like object array.
+But in  javascript we use: document.querySelector("h1").classlist.add("style1")*/
+/* .addClass(): with this method we can add more than one style with the html tag which was created in css file.
+$('h1').addClass('style1 style2');
+$('h1').removeClass('style1 style2'); with this method we can remove any css class from html tag*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- project-basics/inputtypes/formelement.html/practice.js
+++ project-basics/inputtypes/formelement.html/practice.js
@@ -1,5 +1,146 @@
-let name= "Shakhawat Hossain";
-console.log(name);
+// let number = 8;
+
+// console.log(Boolean(number));
+
+
+/* to pick the current time use: let time = new Date().getHours();
+
+let time = new Date().getHours();
+
+
+
+
+if(time >= 6 && time < 12) {
+    console.log('Good Morning')
+
+}
+else if(time >=12 && time < 18){
+    console.log('Good Afternoon');
+
+}
+
+else {
+    console.log('Good Evening');
+
+} */
+
+/* Switch example 1
+let x=0;
+
+switch(x) {
+    case 0:
+        x = "off";
+        break;
+    case 1:
+        x= "on";
+
+} */
+
+/* switch example 2
+let day =4;
+
+switch(day) {
+    case 0:
+        day = "sunday";
+        break;
+    case 1:
+        day= "Monday"
+        break;
+        case 2:
+        day= "Tuesday"
+        break;
+        case 3:
+        day= "Wednesday"
+        break;
+        case 4:
+        day= "Thursday"
+        break;
+        case 5:
+        day= "Friday"
+        break;
+        case 6:
+        day= "Saturday"
+        break;
+        default:
+            day= "Not Found";
+
+        
+
+}
+console.log(day); */
+
+/* switch example 3
+let day =5;
+
+switch(day) {
+    case 0:
+        
+    case 1:
+        
+    case 2:
+        
+    case 3:
+        
+    case 4:
+        day= "It's Office day";
+    
+    break;
+       
+    
+    case 5: 
+    case 6:
+        day= "It's Holiday"
+        
+    break;
+    default:
+        day= "Not Found";
+
+        
+
+}
+
+
+
+
+
+console.log(day); */
+
+/* Switch example 4 daynamic day
+
+let day;
+
+switch(new Date().getDay()) {
+    case 0:
+        
+    case 1:
+        
+    case 2:
+        
+    case 3:
+        
+    case 4:
+        day= "It's Office day";
+    
+    break;
+       
+    
+    case 5: 
+    case 6:
+        day= "It's Holiday"
+        
+    break;
+    default:
+        day= "Not Found";
+
+        
+
+}
+
+
+
+
+
+console.log(day); */
 
 
 
@@ -10,4 +151,131 @@ console.log(name);
 
 
 
-document.getElementById('html').innerHTML= name.length;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(Math.floor(4.00001));
+/*to get the value of sin cos and tan of any degree
+
+ console.log(Math.tan(41 * Math.PI/180)); */
+
+/*  to find the minimum or maximum value
+console.log(Math.max(2,3,4)); */
+
+
+
+// to get random value
+// console.log(Math.random().toFixed(2) *10+1);
+
+/* now make an Lodo app with random
+let upperValue =6;
+let lowerValue =0;
+
+let resultValue= Math.floor(Math.random() * (upperValue - lowerValue)+1);
+
+console.log(resultValue); */
+
+
+/* let now= 2020;
+ let myage = 1990;
+ let fullage = 21;
+ let aliage = 31;
+
+ let average = (fullage + aliage)/2;
+
+
+
+ let  isfullage = now - myage > fullage;*/
+
+
+
+
+ /* function name(firstName, lastName){
+     console.log('Hello ' + firstName + ' ' + lastName);
+ }
+ name('sumon', 'Ahmed');
+ document.getElementById('html').innerHTML= ;
+
+/ function math(number,num2){
+    return number*num2;
+}
+ console.log(math(8,5));
+
+function hello(){
+    document.write('Hello world');
+ } */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let firstName = "Shakhawat";
+// let lastName= prompt("what is your last name?");
+// let age = prompt('what is your  age?')
+// let live = prompt('where do you live?')
+
+
+
+
+// document.getElementById('html').innerHTML= "My name is" + ' ' + firstName + ' ' + lastName + ' ' + ' I am ' + age + ' '  +"years old." + ' ' + 'I live in' + ' ' + live;
+
+// let phone = ['howeie', 'sansung', 'apple'];
+// phone[3]= 'nokia';
+
+
+// document.getElementById('html').innerHTML= phone[0];
+
+
+
+
